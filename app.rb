ad704a1e951dd4e6057685b8496a9fef421e2e5c
Mariam Haider <mfh.haider@gmail.com>
2020-04-30 20:03:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,19 +1,4 @@
-require_relative 'config/environment'
-
-class App < Sinatra::Base
-
-  get '/' do 
-    erb :index
-  end 
-  
-  get 'new' do 
-    erb :create_puppy
-  end 
-  
-  post '/puppy' do
-   @puppy = Puppy.new(params)
-      
-   erb :display_puppy
-   end
-   
+def initialize(params)
+   params.each{|k,v| self.send("#{k}=",v)}
 end
+
