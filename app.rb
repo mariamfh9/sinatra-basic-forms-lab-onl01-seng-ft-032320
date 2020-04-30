16de06713e38b7eba7a5052c6b04d09f458ae54e
Mariam Haider <mfh.haider@gmail.com>
2020-04-30 20:06:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,10 +12,9 @@ class App < Sinatra::Base
       erb :create_puppy
    end
 
-   post '/puppy' do
-   @puppy = Puppy.new(params)
-      
-   erb :display_puppy
-   end
+  post '/puppy' do
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    erb :display_puppy
+  end
 
 end
